0865854d676b4fb3b7f14a1dc4f4ba9aa0f75643
Sonu Kumar <37504411+sonukg@users.noreply.github.com>
2026-01-05 12:25:16 +0530

    Add files via upload

---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by sonukg on 04/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hike/Hike/Utility/ColorExtention.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtention.swift
+//  Hike
+//
+//  Created by sonukg on 04/01/26.
+//
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    static let customGrayDark = Color("ColorGrayDark")
+    static let customIndigoLight = Color("ColorIndigoLight")
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalmonLight = Color("ColorSalmonLight")
+}
