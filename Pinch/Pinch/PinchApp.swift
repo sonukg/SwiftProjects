f8e5f429c3faa874cda546d5bf1754e175815f25
Sonu Kumar <37504411+sonukg@users.noreply.github.com>
2026-01-06 22:13:04 +0530

    Add files via upload

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by sonukg on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
